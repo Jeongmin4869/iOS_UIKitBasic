3c8e5997389e6ac2784dbc906aac68fb1aacc425
jeongmin4869 <ljm4135@naver.com>
2025-04-09 22:21:12 +0900

    Second Commit

---
+++ HelloXcode/HelloXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloXcode
+//
+//  Created by 이정민 on 2025/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
