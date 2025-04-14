77bdbcee64d66de1df4302f5ca297df4c2a755c6
jeongmin4869 <ljm4135@naver.com>
2025-04-14 23:00:47 +0900

    TEXT COMMIT

---
+++ WarmingUp.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+// Token
+
+2 + 3 ;
+
+// Expression
+// 하나 이상의 토큰이 모여서 하나의 값을 도출하는 코드
+
+var x = 7
+x
+
+x + 1
+// Arthmetic Expression
+// 산술 표현식
+
+// Statement
+// 하나 이상의 표현식이 모여서 특정 작업을 진행하는 코드
+// if문, for반복문
+
+7
+// 리터럴
+// Literal
+
+// Identifier
+// 식별자
+let number = 1
+let Number = 1
+let _nunmber = 1
+
+// Keyword
+// 예약어
+// var let = 7
