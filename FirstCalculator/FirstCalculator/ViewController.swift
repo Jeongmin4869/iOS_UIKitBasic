8c7179be52a10c84b373c2e5d7536e64982ae069
jeongmin4869 <ljm4135@naver.com>
2025-04-18 22:03:20 +0900

    COMMIT

--- FirstCalculator/FirstCalculator/ViewController.swift
+++ FirstCalculator/FirstCalculator/ViewController.swift
@@ -9,6 +9,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstOperandField: UITextField!
+    @IBOutlet weak var secondOperandField: UITextField!
+    
+    @IBAction func selectOperator(_ sender: Any) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let plusAction = UIAlertAction(title: "+(더하기)", style: .default) {_ in
+            self.operatorButton.setTitle("+", for: .normal)
+        }
+        actionSheet.addAction(plusAction)
+        
+        let minusAction = UIAlertAction(title: "-(빼기)", style: .default){_ in
+            self.operatorButton.setTitle("-", for: .normal)
+            
+        }
+        actionSheet.addAction(minusAction)
+        
+        let multiplyAction = UIAlertAction(title: "*(곱하기)", style: .default) { _ in
+            self.operatorButton.setTitle("*", for: .normal)
+        }
+        actionSheet.addAction(multiplyAction)
+        
+        let devideAction = UIAlertAction(title: "/(나누기)", style: .default) { _ in
+            self.operatorButton.setTitle("/", for: .normal)
+        }
+        actionSheet.addAction(devideAction)
+        
+        present(actionSheet, animated: true)
+        
+    }
+    
+    
+    @IBOutlet weak var operatorButton: UIButton!
+    
+    @IBOutlet weak var resultLable: UILabel!
+    
+    @IBAction func calculate(_ sender: Any) {
+        
+        let a = Int(firstOperandField.text!)!
+        let b = Int(secondOperandField.text!)!
+        
+        let op = operatorButton.title(for: .normal)!
+        var result = 0
+        
+        if op == "+"{
+            result = a + b
+        } else if op == "-"{
+            result = a - b
+        } else if op == "*"{
+            result = a * b
+        } else if op == "/"{
+            result = a / b
+        } else {
+            print("연산자 선택")
+        }
+                
+        resultLable.text = "\(result)" // String Interpolation
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
