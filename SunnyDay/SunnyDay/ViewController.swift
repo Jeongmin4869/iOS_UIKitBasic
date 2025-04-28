65f0e4bc64068f26eaf86fb0e7fca429db9e1da3
jeongmin4869 <ljm4135@naver.com>
2025-04-28 22:36:59 +0900

    COMMIT

--- SunnyDay/SunnyDay/ViewController.swift
+++ SunnyDay/SunnyDay/ViewController.swift
@@ -15,17 +15,31 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var temperatureLabel: UILabel!
         
+    func getWeatherImage(matching weather: String) -> UIImage? {
+        switch weather {
+        case "맑음" :
+            return UIImage(named: "sun")
+        case "비" :
+            return UIImage(named: "umbrella")
+        default :
+            return nil
+        }
+    }
+    
+    /*
+     nil coalescing operator
+     nil 병합 연산자
+     optional_expr ?? b
+     String? ?? String
+     
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let weather = "맑음"
+        let weather = ["맑음", "비"].randomElement() ?? "맑음"
         let temperature = 17
-        
-        if weather == "맑음"{
-            weatherImageView.image = UIImage(named: "sun")
-        } else if weather == "비" {
-            weatherImageView.image = UIImage(named: "umbrella")
-        }
-        
+        // let temperature = Int.radome(in : -20 ... 38)
+        weatherImageView.image = getWeatherImage(matching: weather)
         statusLabel.text = weather
         temperatureLabel.text = "\(temperature)"
     }
