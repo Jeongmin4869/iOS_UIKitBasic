132119e5429e0a7929056f5613abec86fbd063b9
jeongmin4869 <ljm4135@naver.com>
2025-05-25 22:40:50 +0900

    Enum 활용

---
+++ SunnyDay/SunnyDay/Model/Weather.swift
@@ -0,0 +1,45 @@
+//
+//  Weather.swift
+//  SunnyDay
+//
+//  Created by 이정민 on 2025/05/25.
+//
+
+import UIKit
+
+enum Weather: String {
+    case clear = "맑음"
+    case cloudy = "흐림"
+    case rain = "비"
+    case snow = "눈"
+    
+    var image: UIImage? {
+        switch self {
+            case .clear :
+                return UIImage(named: "sun")
+            case .cloudy :
+                return UIImage(named: "umbrella")
+            case .rain :
+                return UIImage(named: "sun")
+            case .snow :
+                return UIImage(named: "sun")
+        }
+    }
+    
+    func getDescripton(matching temperature: Int) -> String? {
+        switch temperature {
+        case ..<(-10):
+            return "외출조심"
+        case -10 ... 10:
+            return "두꺼운 겉옷"
+        case 11 ... 20:
+            return "일교차 조심"
+        case 21 ... 30:
+            return "초여름 날씨"
+        case 31... :
+            return "더위 조심"
+        default:
+            return nil
+        }
+    }
+}
--- SunnyDay/SunnyDay/ViewController.swift
+++ SunnyDay/SunnyDay/ViewController.swift
@@ -14,17 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var statusLabel: UILabel!
     
     @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
         
-    func getWeatherImage(matching weather: String) -> UIImage? {
-        switch weather {
-        case "맑음" :
-            return UIImage(named: "sun")
-        case "비" :
-            return UIImage(named: "umbrella")
-        default :
-            return nil
-        }
+    /*
+    func getWeatherImage(matching weather: String) -> UIImage? {º
+         switch weather {
+             case "맑음" :
+                 return UIImage(named: "sun")
+             case "비" :
+                 return UIImage(named: "umbrella")
+             default :
+                 return nil
+         }
     }
+    */    
     
     /*
      nil coalescing operator
@@ -36,12 +40,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let weather = ["맑음", "비"].randomElement() ?? "맑음"
-        let temperature = 17
-        // let temperature = Int.radome(in : -20 ... 38)
-        weatherImageView.image = getWeatherImage(matching: weather)
-        statusLabel.text = weather
+        let weather = [Weather.clear, .cloudy, .rain, .snow].randomElement() ?? .clear // 타입추론
+        let temperature = Int.random(in: -10 ... 30)
+        // let temperature = Int.radome(in : -20 ... 38)0
+        weatherImageView.image = weather.image
+        statusLabel.text = weather.rawValue
         temperatureLabel.text = "\(temperature)"
+        descriptionLabel.text = weather.getDescripton(matching: temperature)
     }
 
 
