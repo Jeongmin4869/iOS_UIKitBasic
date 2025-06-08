79976e4957efcc907a04592a0dfc1ddba7111cc6
jeongmin4869 <ljm4135@naver.com>
2025-06-08 22:02:08 +0900

    Delegate Pattern

--- SimpleList/SimpleList/ViewController.swift
+++ SimpleList/SimpleList/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 // UIViewController : 클래스, UITableViewDataSource : 프로토콜 (요구사항, 규칙)
 // swift는 다중상속을 지원하지 않는다.
 
+// Delegate pattern : 하나의 객체가 다른 객체의 일을 대신
+// dataSource : 데이터 제공 객체
+// delegate : 이벤트 처리 객체
+// UITableViewDataSource : TableView가 DataSource의 역할을 할 수 있음을 선언
+
 class ViewController: UIViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -17,9 +22,16 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // #1. 화면에 표시할 셀 생성 -> dequeueReusableCell (재사용 매커니즘) ₩
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "\(indexPath.row)"
+        
+        // #2. 생성된 cell 에 데이터 셋팅
+        cell.textLabel?.text = "\(indexPath.section) - \(indexPath.row)"
+        
+        // #3. 생성된 cell 리턴
         return cell
+        
     }
     
 
