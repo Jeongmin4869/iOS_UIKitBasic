b5e4d758773a4ec94378d05473e8f37581c33533
jeongmin4869 <ljm4135@naver.com>
2025-06-15 18:51:22 +0900

    TableView

--- SimpleList/SimpleList/ViewController.swift
+++ SimpleList/SimpleList/ViewController.swift
@@ -15,31 +15,51 @@ import UIKit
 // delegate : 이벤트 처리 객체
 // UITableViewDataSource : TableView가 DataSource의 역할을 할 수 있음을 선언
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController{// , UITableViewDataSource {
+    
+    let fruits:[String] = ["apple", "mango", "banana", "watermelon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+// extension은 타입확장, 프로토콜 구현에 사용할 수 있다.
+// 기능적으로 분리된 코드 -> 관리가 쉬워짐
+// ViewController의 코드가 길어지면 관리가 힘들어진다.
+// TableView를 사용하지 않을경우 아래 코드만 삭제
+// extension만 다른 파일로 분리 가능
+// Delegate코드는 extension으로 분리하는 것이 좋다.
+extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("#1", #function) // 첫 호출되어 표시할 cell의 갯수를 리턴. 2번 호출됨 (이유는 모름)
         return 100
+        // return fruits.count
     }
     
+    // cellForRowAt : 셀마다 반복적으로 호출되기 때문에 가벼운 코드만 사용
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        // #1. 화면에 표시할 셀 생성 -> dequeueReusableCell (재사용 매커니즘) ₩
+        print("#2", #function, indexPath) // 화면에 표시되는 셀보다 조금 많이 호출됨
+        
+        // #1. 화면에 표시할 셀 생성 -> dequeueReusableCell (재사용 매커니즘. 재사용할 셀이 없다면 생성)
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        // #2. 생성된 cell 에 데이터 셋팅
-        cell.textLabel?.text = "\(indexPath.section) - \(indexPath.row)"
+        // #2. 생성된 cell 에 데이터 셋팅. 셀이 호출되기 직전에 실행
+        if(indexPath.row < 19) {
+            // 셀을 항상 생성하는 것이라면 18 이후의 셀은 모두 text가 표시되지 않을것임.
+            // 실행해보면 스크롤 했을 경우 18이후의 셀 몇개는 text가 표시되지 않고, 다시 0-0으로 돌아간것을 확인 -> 재사용큐에서 가져온 셀
+            // 재사용 된 큐를 계속 사용하고있는것을 확인(재사용큐에 들어간 채로 호출. text를 바꾸지 않으므로 그대로 사용)
+            // 재사용큐에 들어갈 때 index순서대로 들어가지 않기 때문에 호출되는 순서는 다를 수 있음
+            cell.textLabel?.text = "\(indexPath.section) - \(indexPath.row)"
+        }
+        
+        cell.textLabel?.text = "\(indexPath.section) - \(indexPath.row) : \(fruits[indexPath.row%fruits.count])"
         
         // #3. 생성된 cell 리턴
         return cell
         
     }
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
-
