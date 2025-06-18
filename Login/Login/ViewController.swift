e2f2a2adfc501cb67bcbed2e3a26706c3a99adf1
jeongmin4869 <ljm4135@naver.com>
2025-06-18 21:30:46 +0900

    TextField Delegate

--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -53,7 +53,43 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
         idField.becomeFirstResponder()
     }
+}
 
+extension ViewController: UITextFieldDelegate {
+    
+    // 매번 편집이 종료되면 메소드 호출
+    // 값이 정상이라면 원래대로 돌아옴 (isValid 가 true일때 셋팅한 값이 셋팅)
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        
+        if textField == idField {
+            let cnt = textField.text?.count ?? 0 // 옵셔널일 경우 0을 기본값으로
+            let isValidId = (6 ... 12).contains(cnt)
+            
+            textField.layer.borderWidth = isValidId ? 0 : 1
+            textField.layer.borderColor = isValidId ? nil : UIColor.red.cgColor
+            textField.layer.cornerRadius = isValidId ?  0 : 5
+            textField.tintColor = isValidId ? view.tintColor : UIColor.red
+            
+            return isValidId
+        }
+        
+        return true
+    }
+    
+    // return 키 선택 시 호출
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(#function, textField)
+        
+        switch textField {
+            case idField:
+                pwField.becomeFirstResponder()
+            case pwField:
+                login(self) // Action 을 직접 호출할 수 있다.
+            default:
+                break
+        }
+        return false
+    }
 
 }
 
