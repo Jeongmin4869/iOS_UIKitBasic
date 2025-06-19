3153ef13f8ac7f53697fb460e19376fd774bc46a
jeongmin4869 <ljm4135@naver.com>
2025-06-19 23:11:37 +0900

    Input TextField

--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
     
+    @IBOutlet weak var LoginButton: UIButton!
+    
     // 암기하기 좋음
     func showAlert(message: String){
         let alert = UIAlertController(title: "알림", message: message, preferredStyle: .alert)
@@ -56,6 +58,27 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITextFieldDelegate {
+    
+    // 사용자가 키보드를 통해 키를 입력하면 호출(입력 반영 전 호출). true를 리턴하면 실제로 입력반영. false라면 반영 안함
+    // true를 리턴할 경우 3번째 파라미터인 string으로 반영이 됨(최종 텍스트)
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // print(#function, textField.text, string) // 텍스트가 한글자씩 늦게 호출되는것을 확일 할 수 있음
+        
+        // 최종 텍스트를 얻는 방법
+        // NSRange -> Range 타입으로 변경
+        guard let text = textField.text, let range = Range(range, in:text) else {
+            return true
+        }
+        let finalText = text.replacingCharacters(in: range, with: string)
+        print(finalText) // -> 최종 텍스트를 출력
+        
+        let pwText =
+        
+        LoginButton.isEnabled = !finalText.isEmpty 
+        
+        return true // false일 경우 글자 변경이 반영되지 않음
+    }
     
     // 매번 편집이 종료되면 메소드 호출
     // 값이 정상이라면 원래대로 돌아옴 (isValid 가 true일때 셋팅한 값이 셋팅)
