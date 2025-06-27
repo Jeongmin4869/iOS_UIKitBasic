e0c2b3244c16cfef8f4c6303c0b9765f6f9d8fa5
jeongmin4869 <ljm4135@naver.com>
2025-06-27 21:14:32 +0900

    TableView

---
+++ JustToDo/JustToDo/AddViewController.swift
@@ -0,0 +1,50 @@
+//
+//  AddViewController.swift
+//  JustToDo
+//
+//  Created by 이정민 on 2025/06/27.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet weak var inputField: UITextField!
+    // present : 모달의 화면을 열음
+    // dismiss : 모달의 화면을 닫음
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true) // 애니메이션 추가
+    }
+    @IBAction func save(_ sender: Any) {
+        
+        // 1. 입력한 문자열 가져오기
+        guard let text = inputField.text else{
+            return
+        }
+        
+        // 2. 텍스트 저장
+        
+        
+        // 3. 화면 없애기
+        dismiss(animated: true)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
