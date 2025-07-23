c73b4dfb549df2b7fc1e8c3a30dbf661729233c6
jeongmin4869 <ljm4135@naver.com>
2025-07-23 22:33:40 +0900

    Delegate 활용

--- AppLifeCycle/AppLifeCycle/ViewController.swift
+++ AppLifeCycle/AppLifeCycle/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 브라우저로 이동
+    @IBAction func openBrowser(_ sender: Any) {
+        
+        // url로 이동
+        // guard let url = URL(string :"https://www.naver.com") else { return }
+        
+        // 앱 설정으로 이동
+        guard let url = URL(string :UIApplication.openSettingsURLString) else { return }
+        
+        if UIApplication.shared.canOpenURL(url){
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    /*--------------------------------------*/
+    /*         라이플 사이클 이벤트 메소드         */
+    /*--------------------------------------*/
+    // sceneDidBecomeActive : Active상태가 된 이후 호출
+    // sceneDelegate에서 가져온 메소드
+    // 과연 호출이 될까 ? -> 호출되지 않음
+    // SceneDelegate은 씬을 만들 때 함께 만들어지고, Delegate로 지정된다.
+    // 이벤트가 발생하였을 때 Scene 객체는 SceneDelegate에 있는 메소드를 호출
+    // ViewController는 UISceneDelegate를 구현하고 있지않음. 프로토콜을 채용하지도, Delegate로 선언하지도 않음
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        print(self, #function)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Application 인스턴스에 접근
-        // 앱델리게이트에 접근하기위한 수단
+        // 앱 델리게이트에 접근하기위한 수단
 //        UIApplication.shared
         // 사용하기 위해 AppDelegate로 타입 캐스팅
         if let appDelegate = UIApplication.shared.delegate as? AppDelegate{
@@ -33,12 +62,35 @@ class ViewController: UIViewController {
             sceneDelegate.sharedData
         }
         
-        
+        // AppDelegate 사용 예
+        UIApplication.shared.isIdleTimerDisabled = false; // 화면 대기상태로 이동하는 기능을 Off
         
         // 아래와 같은 속성들은 대부분 공유객체를 리턴한다.
 //        URLSession.shared
 //        Notification.default
 //        UserDefaults.standard
+        
+        
+        /*--------------------------------------*/
+        /*         라이플 사이클 이벤트 메소드         */
+        /*--------------------------------------*/
+        // Active상태가 되었을 때 ViewController 에서 호출되는 메소드
+        // UIScene.didActivateNotification : iOS 13 부터 새롭게 채용된 메소드
+        // SceneDelegate가 아닌 다른 코드에서 라이프사이클 이벤트를 처리하고 싶다면 Notification을 사용
+        /*
+         <AppLifeCycle.SceneDelegate: 0x600002675ea0> scene(_:willConnectTo:options:)
+         <AppLifeCycle.SceneDelegate: 0x600002675ea0> sceneWillEnterForeground(_:)
+         <AppLifeCycle.SceneDelegate: 0x600002675ea0> sceneDidBecomeActive(_:)
+         NSNotificationName(_rawValue: UISceneDidActivateNotification) -> ViewController에서 추가된 코드 
+         
+         
+         */
+        NotificationCenter.default.addObserver(forName: UIScene.didActivateNotification, object: nil, queue: .main ) { _ in
+            print(UIScene.didActivateNotification)
+        }
+        
+        
+        
     }
 
 
