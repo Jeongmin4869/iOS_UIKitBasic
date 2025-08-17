23d2a9773be567abc97bb060f235fa85b02c4ba5
jeongmin4869 <ljm4135@naver.com>
2025-08-17 18:53:58 +0900

    Modal

---
+++ ScreenToView/ScreenToView/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  ScreenToView
+//
+//  Created by 이정민 on 2025/08/17.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
